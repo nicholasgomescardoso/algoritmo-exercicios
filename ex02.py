45cf29d8ab0f3ff5082dff47b8408aa912528b43
Nicholas <nicholascardoso.dev@gmail.com>
2026-09-14 23:08:47 -0300

    atividade 2

---
+++ ex02.py
@@ -0,0 +1,16 @@
+# Exercicio 02 - Calculo do carrinho de compras]
+
+titulo = "Calculo do carrinho de compras"
+preco = 25.00
+quantidade = 3
+desconto = 5.00
+
+subtotal = preco * quantidade
+total = subtotal - desconto
+
+print(f' {titulo}')
+print(f'Preço: R$ {preco:.2f}')
+print(f'Quantidade: {quantidade}')
+print(f'Subtotal: R$ {subtotal:.2f}')
+print(f'Desconto: R$ {desconto:.2f}')
+print(f'Total: R$ {total:.2f}')
