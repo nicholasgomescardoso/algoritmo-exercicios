d45a36950cd25faa4d8e5ee461f31b617cf9c105
Nicholas <nicholascardoso.dev@gmail.com>
2026-09-14 23:04:42 -0300

    atividade 1

---
+++ ex01.py
@@ -0,0 +1,39 @@
+# Exercicio 01 - Tipos, variaveis e expressoes em Python
+print()
+nome = 'Python'
+
+print(nome)
+print()
+
+resultado = 2 + 3 * 4
+#14
+print(resultado)
+print()
+
+mensagem = 'Olá, Python'
+print(mensagem)
+print()
+
+conta = 2 + 3 * 4
+print(conta)
+print()
+
+explicacao_conta = '2 + 3 * 4 = 14'
+print(explicacao_conta)
+print()
+
+texto = explicacao_conta + ' = ' + str(conta)
+print(texto)
+print()
+
+texto2 = f'{explicacao_conta} = {conta}'
+print(texto2)
+print()
+
+print(type(nome))
+print(type(resultado))
+print(type(mensagem))
+
+
+
+
